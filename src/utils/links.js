6276b11f844b59e6299f4c36545c3329295e64f3
Barun Kumar <77458180+thebarunkumar@users.noreply.github.com>
2023-10-13 14:58:32 +0530

    Add files via upload

---
+++ src/utils/links.js
@@ -0,0 +1,7 @@
+export const social = {
+    github: 'https://github.com/thebarunkumar',
+    linkedin: 'https://www.linkedin.com/in/thebarunkumar/',
+    portfolio: 'https://thebarunkumar-portfolio.vercel.app/'
+};
+
+
